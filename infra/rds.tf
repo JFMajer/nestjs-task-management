8daeef52d1b1a21749a3f8e8bccc5bd58a0fdb8f
JFMajer <jakubmajer@live.com>
2023-09-28 19:25:58 +0200

    rds

---
+++ infra/rds.tf
@@ -0,0 +1,56 @@
+locals {
+    name = "postgres"
+    tags = {
+        Name = local.name
+    }
+}
+
+module "rds" {
+    source = "terraform-aws-modules/rds/aws"
+    version = "~> 6.0"
+
+    engine = "postgres"
+    engine_version = "15"
+    family = "postgres15"
+    major_engine_version = "15"
+    instance_class       = "db.t4g.small"
+
+    allocated_storage = 20
+
+    db_name = "task_management"
+    db_username = "postgres"
+    port = 5432
+
+    multi_az = false
+    storage_type = "gp3"
+
+    db_subnet_group_name = module.vpc.database_subnet_group
+    vpc_security_group_ids = [module.security_group.security_group_id]
+
+    tags = local.tags        
+    
+}
+
+
+
+module "security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 5.0"
+
+  name        = "${local.name}-sg"
+  description = "Allow access from within VPC"
+  vpc_id      = module.vpc.vpc_id
+
+  # ingress
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 5432
+      to_port     = 5432
+      protocol    = "tcp"
+      description = "PostgreSQL access from within VPC"
+      cidr_blocks = module.vpc.vpc_cidr_block
+    },
+  ]
+
+  tags = local.tags
+}
