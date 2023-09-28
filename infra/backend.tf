eb0112af406d632c6c9b5e0dd663835a6c317936
JFMajer <jakubmajer@live.com>
2023-09-28 18:03:57 +0200

    added infra dir, testing if workflow will run

---
+++ infra/backend.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 0.1.0, < 2.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "#{S3_BUCKET}#"
+    key            = "task-management-infra/terraform.tfstate"
+    region         = "#{AWS_REGION}#"
+    dynamodb_table = "#{DYNAMO_TABLE}#"
+    encrypt        = true
+  }
+}
