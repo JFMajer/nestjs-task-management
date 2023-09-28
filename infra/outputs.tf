5ffa7fb81afeab1beabbf42dd3f165b0c0128a1d
JFMajer <jakubmajer@live.com>
2023-09-28 20:30:23 +0200

    better outputs

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,9 +1,9 @@
 output "rds_dns_name" {
   description = "The DNS name of the RDS instance"
-  value       = aws_db_instance.this.endpoint
+  value       = module.rds.db_instance_endpoint
 }
 
 output "rds_secret_arn" {
   description = "The ARN of the RDS secret in Secrets Manager"
-  value       = aws_db_instance.this.associated_secret[0].arn
+  value       = module.rds.db_instance_master_user_secret_arn
 }
