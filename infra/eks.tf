a2b2f6e5edf4368eacdae28276034162d9abf144
JFMajer <jakubmajer@live.com>
2023-09-29 19:42:35 +0200

    deploy cluster

--- infra/eks.tf
+++ infra/eks.tf
@@ -195,6 +195,6 @@ module "eks-load-balancer-controller" {
   version = "1.2.0"
   
   cluster_name = module.eks.cluster_name
-  cluster_oidc_issuer_url = module.eks.oidc_provider_arn
-  cluster_oidc_issuer_client_id = module.eks.oidc_provider_arn
+  cluster_identity_oidc_issuer_arn = module.eks.oidc_provider_arn
+  cluster_identity_oidc_issuer = module.eks.oidc_provider_arn
 }
