430b2439b9bfd6935fc8cdb324c9157603206d0d
JFMajer <jakubmajer@live.com>
2023-09-30 18:23:51 +0200

    alb controller module

--- infra/alb-controller.tf
+++ infra/alb-controller.tf
@@ -1,8 +1,17 @@
-# module "eks-load-balancer-controller" {
-#   source  = "lablabs/eks-load-balancer-controller/aws"
-#   version = "1.2.0"
+module "eks-load-balancer-controller" {
+  source = "git::https://github.com/DNXLabs/terraform-aws-eks-lb-controller.git"
 
-#   cluster_name                     = module.eks.cluster_name
-#   cluster_identity_oidc_issuer_arn = module.eks.oidc_provider_arn
-#   cluster_identity_oidc_issuer     = module.eks.oidc_provider_arn
+  enabled = true
+
+  cluster_identity_oidc_issuer     = module.eks.cluster_oidc_issuer_url
+  cluster_identity_oidc_issuer_arn = module.eks.oidc_provider_arn
+  cluster_name                     = module.eks.cluster_id
+
+  depends_on = [module.eks]
+}
+
+# resource "aws_iam_policy" "alb_controller_policy" {
+#     name        = "alb-controller-policy"
+#     description = "Policy for ALB Controller"
+#     policy      = file("iam_policy.json")
 # }
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,9 +1,9 @@
 output "rds_dns_name" {
-  description = "The DNS name of the RDS instance"
+  description = "The DNS name of the RDS instance, with port"
   value       = module.rds.db_instance_endpoint
 }
 
-output "rds_secret_arn" {
-  description = "The ARN of the RDS secret"
-  value       = module.rds.db_instance_master_user_secret_arn
+output "instance_address" {
+  description = "The address of the RDS instance"
+  value       = module.rds.db_instance_address
 }
