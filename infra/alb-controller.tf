438cc70d7524c8abb76f749bc13d93097182516f
JFMajer <jakubmajer@live.com>
2023-10-01 16:17:06 +0200

    OIDC issuer output

--- infra/alb-controller.tf
+++ infra/alb-controller.tf
@@ -5,35 +5,35 @@ resource "aws_iam_policy" "alb_controller_policy" {
 }
 
 data "aws_iam_policy_document" "load-balancer-role-trust-policy" {
-    statement {
-        effect = "Allow"
-        actions = [
-            "ats:AssumeRoleWithWebIdentity"
-        ]
-        principals {
-            type        = "Federated"
-            identifiers = [module.eks.oidc_provider_arn]
-        }
-        condition {
-            test = "StringEquals"
-            variable = "oidc.eks.eu-north-1.amazonaws.com/id/${module.eks.oidc_provider}:aud"
-            values = [
-                "sts.amazonaws.com"
-            ]
-        }
-        condition {
-            test = "StringEquals"
-            variable = "oidc.eks.eu-north-1.amazonaws.com/id/${module.eks.oidc_provider}:sub"
-            values = [
-                "system:serviceaccount:kube-system:aws-load-balancer-controller"
-            ]
-        }
+  statement {
+    effect = "Allow"
+    actions = [
+      "ats:AssumeRoleWithWebIdentity"
+    ]
+    principals {
+      type        = "Federated"
+      identifiers = [module.eks.oidc_provider_arn]
     }
-} 
+    condition {
+      test     = "StringEquals"
+      variable = "oidc.eks.eu-north-1.amazonaws.com/id/${module.eks.oidc_provider}:aud"
+      values = [
+        "sts.amazonaws.com"
+      ]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "oidc.eks.eu-north-1.amazonaws.com/id/${module.eks.oidc_provider}:sub"
+      values = [
+        "system:serviceaccount:kube-system:aws-load-balancer-controller"
+      ]
+    }
+  }
+}
 
 resource "aws_iam_role" "load-balancer-role" {
-    name = "aws-load-balancer-controller"
-    assume_role_policy = aws_iam_policy_document.load-balancer-role-trust-policy.json
+  name               = "aws-load-balancer-controller"
+  assume_role_policy = data.aws_iam_policy_document.load-balancer-role-trust-policy.json
 }
 
 resource "aws_iam_role_policy_attachment" "alb_controller_policy_attachment" {
@@ -43,7 +43,7 @@ resource "aws_iam_role_policy_attachment" "alb_controller_policy_attachment" {
 
 resource "kubernetes_service_account" "aws_load_balancer_controller" {
   metadata {
-    name = "aws-load-balancer-controller"
+    name      = "aws-load-balancer-controller"
     namespace = "kube-system"
 
     labels = {
