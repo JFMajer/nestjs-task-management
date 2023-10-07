2aad566f5c621b5c1e5704953170cd4f50d06246
JFMajer <jakubmajer@live.com>
2023-10-07 17:30:25 +0200

    AMI type is not needed i think [skip ci]

--- infra/eks.tf
+++ infra/eks.tf
@@ -41,7 +41,7 @@ module "eks" {
   manage_aws_auth_configmap = true
 
   eks_managed_node_group_defaults = {
-    ami_type                   = "AL2_x86_64"
+    # ami_type                   = "AL2_x86_64"
     iam_role_attach_cni_policy = true
   }
 
