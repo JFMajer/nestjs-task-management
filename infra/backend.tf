c39b6ae495a8150230eb1a1a7364a14d67f016bf
JFMajer <jakubmajer@live.com>
2023-10-21 12:03:39 +0200

    added kubectl to required providers

--- infra/backend.tf
+++ infra/backend.tf
@@ -13,6 +13,10 @@ terraform {
       source  = "hashicorp/helm"
       version = "~> 2.0"
     }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~> 1.14.0"
+    }
   }
 
   backend "s3" {
