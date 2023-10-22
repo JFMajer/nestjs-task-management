df6965c55e8dda5fd48c1c69c1503960305e2f91
JFMajer <jakubmajer@live.com>
2023-10-22 13:38:29 +0200

    new karpenter helm chart version

--- infra/eks.tf
+++ infra/eks.tf
@@ -63,6 +63,13 @@ module "eks" {
   }
 
   fargate_profiles = {
+    kube_system = {
+      selectors = [
+        {
+          namespace = "kube-system"
+        }
+      ]
+    }
     karpenter = {
       name = "karpenter"
       selectors = [
